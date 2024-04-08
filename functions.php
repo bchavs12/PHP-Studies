b5fee19a3a4fef1259b9adfb37671b95da922094
Bruno Chaves <chavesbruno699@gmail.com>
2024-04-08 02:18:26 -0300

    php dynamicWebRouter

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function dd($value) {
+function dd($value)
+{
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
@@ -8,6 +9,25 @@ function dd($value) {
     die();
 }
 
-function urlIs($value){
-    return $_SERVER['REQUEST_URI'] == "$value";
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI'] === "$value";
+}
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
 }

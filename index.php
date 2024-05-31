ccebd56e2431854bc3b2caae14b1547ccd7cd3f2
bchavs12 <chavesbruno699@gmail.com>
2024-05-31 16:20:44 -0300

    section 2: Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 require "functions.php";
-require "router.php";
+require "Database.php";
+
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}

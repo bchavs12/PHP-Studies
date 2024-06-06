c2caa58a787ba35d61778b1ca3eada79fa7a48b7
bchavs12 <chavesbruno699@gmail.com>
2024-06-06 01:15:03 -0300

    section 3: authorization, status Code, magic numbers

---
+++ Response.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Magic Numbers:
+ * A unique value with unexplained meaning or multiple occurrences which could (preferably) be replaced with a named constant.
+ */
+class Response
+{
+  const NOT_FOUND = 404;
+  const FORBIDDEN = 403;
+}
--- controllers/note.php
+++ controllers/note.php
@@ -3,17 +3,20 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = "Note";
+$currentUserId = 1;
 
 $note = $db->query("SELECT * FROM notes where id = :id", [
   'id' => $_GET['id']
 ])->fetch();
 
+// if we couldn't find any matching note from the database
 if (!$note) {
   abort();
 }
 
-if ($note['user_id'] !== 1) {
-  abort(403);
+// if the note wasn't created by the current user it happens
+if ($note['user_id'] !== $currentUserId) {
+  abort(Response::FORBIDDEN);
 }
 
 require 'views/note.view.php';

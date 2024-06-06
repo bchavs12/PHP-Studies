bc5edafc35a8973da92eed956ab5b5aa94e1654b
bchavs12 <chavesbruno699@gmail.com>
2024-06-06 00:11:15 -0300

    section 3: Introduction to Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,6 +4,16 @@ $db = new Database($config['database']);
 
 $heading = "Note";
 
-$note = $db->query("SELECT * FROM notes where id = :id", ['id' => $_GET['id']])->fetch();
+$note = $db->query("SELECT * FROM notes where id = :id", [
+  'id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+  abort();
+}
+
+if ($note['user_id'] !== 1) {
+  abort(403);
+}
 
 require 'views/note.view.php';
